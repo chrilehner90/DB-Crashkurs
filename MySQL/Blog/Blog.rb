7d303897d6ab109348ff01710057264351a07e09
Christian Lehner <lehner.chri@gmail.com>
2015-10-16 01:33:59 +0200

    added Blog code example

--- MySQL/Blog/Blog.rb
+++ MySQL/Blog/Blog.rb
@@ -6,6 +6,8 @@ begin
 	connection.query "drop database if exists Blog"
 	connection.query "create database Blog"
 
+	connection.query "use Blog"
+
 	result = connection.query "show databases"
 
 	n_rows = result.num_rows
@@ -14,10 +16,52 @@ begin
 		p result.fetch_row.join ""
 	end
 
-	
+	connection.query("CREATE TABLE authors(
+		id INTEGER AUTO_INCREMENT PRIMARY KEY,
+		name VARCHAR(30) NOT NULL
+	);")
+
+	connection.query("CREATE TABLE posts(
+		id INTEGER AUTO_INCREMENT PRIMARY KEY,
+		title VARCHAR(256) NOT NULL,
+		author_id INTEGER,
+		body TEXT NOT NULL,
+		FOREIGN KEY(author_id) REFERENCES authors(id)
+	);")
+
+	connection.query("CREATE TABLE sites(
+		id INTEGER AUTO_INCREMENT PRIMARY KEY,
+		title VARCHAR(256) NOT NULL,
+		author_id INTEGER UNIQUE NOT NULL,
+		FOREIGN KEY(author_id) REFERENCES authors(id)
+	);")
+
+	author = connection.prepare("INSERT INTO authors(name) VALUES (?);")
+	author.execute("Hans Huber")
+	author = connection.prepare("INSERT INTO authors(name) VALUES (?);")
+	author.execute("Johann Goethe")
+	author = connection.prepare("INSERT INTO authors(name) VALUES (?);")
+	author.execute("Douglas Adams")
+	author = connection.prepare("INSERT INTO authors(name) VALUES (?);")
+	author.execute("Douglas Crockford")
 
 
-	
+	# SQL injection example
+	# author = connection.prepare("delete from authors where id = ?")
+	# author.execute("1 OR 1 = 1")
+
+	# author = connection.query("delete from authors where id = 1 OR 1 = 1")
+
+
+	result = connection.query "select * from authors"
+
+	n_rows = result.num_rows
+
+	n_rows.times do
+		p result.fetch_row.join "\s"
+	end
+
+
 
 rescue Mysql::Error => e
 	puts e
